10d66c83d8a71e93700ab43b45cf1df11ef762d8
Ubuntu <molnarandre@outlook.com>
2021-11-19 13:51:32 +0100

    date, device type picket

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -38,38 +38,93 @@
         </v-card-title>
         <v-card-text>
           <v-row>
-            <v-col cols="6">
-              <v-text-field label="ID" v-model="focusItem.id"></v-text-field>
-              <v-text-field
-                label="Device"
-                v-model="focusItem.device"
-              ></v-text-field>
-              <v-text-field
-                label="Type"
-                v-model="focusItem.type"
-              ></v-text-field>
-              <v-text-field
-                label="In stock data"
-                v-model="focusItem.in_stock_data"
-              ></v-text-field>
-            </v-col>
             <v-col cols="6" v-if="focusItem.who_have_it">
               <v-text-field
                 label="email"
                 v-model="focusItem.who_have_it.email"
               ></v-text-field>
               <v-text-field
-                label="First name"
-                v-model="focusItem.who_have_it.first"
-              ></v-text-field>
-              <v-text-field
                 label="Last name"
                 v-model="focusItem.who_have_it.last"
               ></v-text-field>
               <v-text-field
-                label="Kiadva"
-                v-model="focusItem.issued"
+                label="First name"
+                v-model="focusItem.who_have_it.first"
               ></v-text-field>
+              <v-dialog
+                ref="dialog"
+                v-model="modal"
+                :return-value.sync="date"
+                persistent
+                width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="focusItem.issued"
+                    label="Kiadva"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="focusItem.issued" scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="modal = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.dialog.save(focusItem.issued)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-dialog>
+            </v-col>
+            <v-col cols="6">
+              <v-select
+                label="Device type"
+                :items="device_types"
+                v-model="focusItem.device"
+              ></v-select>
+              <v-text-field label="ID" v-model="focusItem.id"></v-text-field>
+              <v-text-field
+                label="Type"
+                v-model="focusItem.type"
+              ></v-text-field>
+              <v-dialog
+                ref="dialog"
+                v-model="modal"
+                :return-value.sync="date"
+                persistent
+                width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="focusItem.in_stock_date"
+                    label="In stock date"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="focusItem.in_stock_date" scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="modal = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.dialog.save(focusItem.in_stock_date)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-dialog>
             </v-col>
           </v-row>
         </v-card-text>
@@ -82,6 +137,8 @@
 export default {
   data() {
     return {
+      device_types: ["Laptop", "Mobile", "SIM"],
+      modal: false,
       status_select: ["Kiadva", "Raktáron", "Rendelés alatt"],
       showDialog: false,
       focusItem: {},
@@ -92,20 +149,20 @@ export default {
           id: "1237182371973981732",
           device: "Laptop",
           type: "Dell 123asd321",
-          in_stock_data: "2021.05.05",
+          in_stock_date: "2021-05-05",
           who_have_it: {
             email: "molnar.andrej@damit.hu",
             first: "Andrej",
             last: "Molnár",
           },
-          issued: "2021.10.11",
+          issued: "2021-10-11",
         },
         {
           status: "Raktáron",
           id: "1823189738127398",
           device: "Laptop",
           type: "HP 8u21asdio",
-          in_stock_data: "2021.06.17",
+          in_stock_date: "2021-06-17",
           who_have_it: {
             email: "",
             first: "",
@@ -116,15 +173,15 @@ export default {
         {
           status: "Kiadva",
           id: "1371298378129397812",
-          device: "Telefon",
+          device: "Mobile",
           type: "Xiaomi 9",
-          in_stock_data: "2020.05.12",
+          in_stock_date: "2020-05-12",
           who_have_it: {
             email: "nagy.david@damit.hu",
             first: "Dávid",
             last: "Nagy",
           },
-          issued: "2020.10.11",
+          issued: "2020-10-11",
         },
       ],
       headers: [
